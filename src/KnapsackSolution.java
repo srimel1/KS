7cf77d84dec1650579ce87a09be16127010629ae
srimel1 <47188816+srimel1@users.noreply.github.com>
2020-02-09 00:38:31 -0500

    Initial commit practice projects

---
+++ src/KnapsackSolution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class KnapsackSolution {
+
+    String approach;
+    public List<Item> items;
+    public double weight;
+    public double value;
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(approach);
+        builder.append(": ");
+        builder.append(value);
+        builder.append(" ");
+        builder.append(weight);
+        builder.append("\n");
+
+        Collections.sort(items, Item.byLabel());
+
+        for (Item item : items) {
+            builder.append(item.label);
+            builder.append(" ");
+        }
+
+        return builder.toString();
+    }
+}
